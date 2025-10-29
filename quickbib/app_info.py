4518afa7db9ba79479bef0fb86a940c0a86700e6
Archisman Panigrahi <apandada1@gmail.com>
2025-10-29 18:52:04 -0400

    add automatic version number updater

--- quickbib/app_info.py
+++ quickbib/app_info.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 # Application metadata
 APP_NAME = "QuickBib"
-APP_VERSION = "0.2"
+APP_VERSION = "0.3"
 HOMEPAGE = "https://github.com/archisman-panigrahi/quickbib"
 REPO_URL = HOMEPAGE
 # LICENSE will be installed alongside the package; point to package-relative file
