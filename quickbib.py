afe02338b7a24b6d12a13095a3c0996310fabd10
Archisman Panigrahi <apandada1@gmail.com>
2025-10-28 22:41:01 -0400

    Update quickbib.py

--- quickbib.py
+++ quickbib.py
@@ -16,7 +16,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
-import sys
 from pathlib import Path
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QIcon
